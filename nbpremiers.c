9bba0de19a480220105429013b7f9b5bd2863586
404Fafnir <heikocarlier@gmail.com>
2023-02-27 14:50:21 +0100

    nb premiers qui fonctionne

--- nbpremiers.c
+++ nbpremiers.c
@@ -6,12 +6,47 @@ void main(){
 
     int nb;
 
-    printf("Quel nombre voulez vous vérifier ?");
+    printf("Quel nombre voulez vous vérifier ? ");
     scanf("%d", &nb);
 
 
-    int racineCaree = floor(sqrt(nb));
+    int limiteRecherche = floor(sqrt(nb));
 
+    int i = 3;
+
+    char isPrime = 0;
+
+    switch (nb%2 == 0)
+    {
+    case 0:
+        while ((i < limiteRecherche || !isPrime) && i < nb)
+        {
+            if (nb%i == 0)
+            {
+                isPrime = 1;    
+            }
+            i++;
+            
+        }
+
+        switch (isPrime)
+        {
+        case 0:
+            printf("%d est premier", nb);
+            break;
+        
+        default:
+            printf("%d n'est pas premier car divisible par %d", nb, i);
+            break;
+        }
+        
+        
+        break;
+    
+    default:
+        printf("%d n'est pas premier", nb);
+        break;
+    }
     
 }
 
